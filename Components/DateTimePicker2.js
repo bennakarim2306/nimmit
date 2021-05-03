eb26b6d1e1ae9757e22a1a03d7e0f375b4be437b
Slim Masmoudi <slimmasmoudi@ymail.com>
2021-05-03 14:56:17 +0100

    datetimepicker date & time

--- Components/DateTimePicker2.js
+++ Components/DateTimePicker2.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Button, View } from "react-native";
+import { Button, View, Text } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const DateTimePicker2 = () => {
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [datetime, setDatetime] = useState(new Date());
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -13,20 +14,29 @@ const DateTimePicker2 = () => {
     setDatePickerVisibility(false);
   };
 
-  const handleConfirm = (date) => {
-    console.warn("A date has been picked: ", date);
+  const handleConfirm = (datetime) => {
+    // console.warn("A date has been picked: ", date);
+    setDatetime(datetime);
     hideDatePicker();
+  };
+
+  const formatDate = (datetime) => {
+    return `${datetime.getDate()}/${
+      datetime.getMonth() + 1
+    }/${datetime.getFullYear()} ${datetime.getHours()}:${datetime.getMinutes()}`;
   };
 
   return (
     <View>
-      <Button title="Choose date" onPress={showDatePicker} />
+      <Button title="Choose date and time" onPress={showDatePicker} />
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
-        mode="date"
+        mode="datetime"
+        minimumDate={Date.parse(new Date())}
         onConfirm={handleConfirm}
         onCancel={hideDatePicker}
       />
+      <Text>{formatDate(datetime)}</Text>
     </View>
   );
 };
